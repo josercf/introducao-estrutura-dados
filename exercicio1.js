0325577fdced0f487cf603709661e617a2309a80
José Romualdo <joseromualdo@outlook.com>
2025-04-17 00:56:29 +0000

    oedenação

--- exercicio1.js
+++ exercicio1.js
@@ -1,12 +1,22 @@
 let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
 
 // percorrer todos os itens do array
-for (let i = 0; i < dados.length; i++) {
-    console.log("O índice é: " +
-                i +
-                " o valor é: " +
-                dados[i] );    
-}
+let ordenado = false
+do 
+{
+    for (let i = 0; i < dados.length; i++) {    
+        let aux = 0;
+        if(dados[i] > dados[i + 1]){
+            console.log(`$[${dados[i]}] é maior que $[${dados[i + 1]}]. Realizando inversão`);
+            aux = dados[i];
+            dados[i] = dados[i + 1];
+            dados[i + 1] = aux;
+        }  
+        console.table(dados);
+    }
+}while(!ordenado);
+
+
 
 //para cada item, verificar se ele é maior que o próximo.
 
