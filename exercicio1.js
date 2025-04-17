8da67a3cd383ae3fbe455b7f6002e48c7c0ffbcf
José Romualdo <joseromualdo@outlook.com>
2025-04-17 00:32:32 +0000

    codificar exercicio 01

---
+++ exercicio1.js
@@ -0,0 +1,19 @@
+let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
+
+// percorrer todos os itens do array
+for (let i = 0; i < dados.length; i++) {
+    console.log("O índice é: " +
+                i +
+                " o valor é: " +
+                dados[i] );    
+}
+
+//para cada item, verificar se ele é maior que o próximo.
+
+//Se for, inverter ambos os valores.
+
+//Caso contrário, não fazer nada e avançar para o próximo.
+
+console.log(dados);
+
+
